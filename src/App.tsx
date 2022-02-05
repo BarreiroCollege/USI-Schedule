355620ef5b5f8ea467ba2c8dd2dff69edd924c83
Diego Barreiro <diego.barreiro.perez@gmail.com>
2022-02-05 17:15:32 +0100

    Finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'antd/dist/antd.css';
+import {Col, Layout, Row} from "antd";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from '@fullcalendar/daygrid';
+import iCalendarPlugin from '@fullcalendar/icalendar';
+import listPlugin from '@fullcalendar/list';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import "./App.css";
+import {COLORS} from "./colors";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const SCHEDULES = [
+  "https://search.usi.ch/en/educations/30/bachelor-of-science-in-informatics/schedules/52/1",
+  "https://search.usi.ch/en/educations/30/bachelor-of-science-in-informatics/schedules/52/2",
+  "https://search.usi.ch/en/educations/30/bachelor-of-science-in-informatics/schedules/52/3",
+];
+
+const COURSES = [
+  "Software Atelier 2",
+  "Computer Networking",
+  "Software Atelier 4",
+  "Machine Learning",
+  "Theory of Computation",
+]
+
+let i = 0;
+
+class App extends React.Component {
+  render() {
+    return <Layout>
+      <Layout.Header/>
+      <Layout.Content>
+        <Row justify="space-around">
+          <Col xs={24} lg={18}>
+            <FullCalendar
+              viewClassNames={'fc'}
+              headerToolbar={{
+                left: 'prev,next today',
+                center: 'title',
+                right: 'Schedule,Week,Month'
+              }}
+              plugins={[dayGridPlugin, timeGridPlugin, listPlugin, iCalendarPlugin]}
+              initialView={window.innerWidth <= 768 ? "Schedule" : "Week"}
+              firstDay={1}
+              timeZone={"local"}
+              nowIndicator={true}
+              views={{
+                Schedule: {type: 'listWeek', duration: {days: 10}},
+                Week: {type: 'timeGridWeek', weekends: false, allDaySlot: false},
+                Month: {type: 'dayGridMonth'},
+              }}
+              slotMinTime={"08:00"}
+              slotMaxTime={"19:00"}
+              businessHours={[{daysOfWeek: [1, 2, 3, 4, 5], startTime: "08:30", endTime: "18:30"}]}
+              eventSources={SCHEDULES.map(s => ({
+                url: `https://cors-pc-sdc-gal.herokuapp.com/${s}/ics`,
+                format: 'ics'
+              }))}
+              eventDataTransform={(e) => {
+                if (!COURSES.find(c => e.title && e.title.startsWith(c))) {
+                  return {};
+                }
+
+                e.color = COLORS[i];
+                i++;
+                return e;
+              }}
+              eventClick={info => {
+                info.jsEvent.preventDefault();
+                if (info.event.url) window.open(info.event.url, "_blank");
+              }}
+            />
+          </Col>
+        </Row>
+      </Layout.Content>
+      <Layout.Footer/>
+    </Layout>;
+  }
 }
 
 export default App;
---
+++ src/colors.ts
@@ -0,0 +1,21 @@
+const red        = "#f44336";
+const pink       = "#f44336";
+const purple     = "#e91e63";
+const deepPurple = "#9c27b0";
+const indigo     = "#673ab7";
+const blue       = "#3f51b5";
+const lightBlue  = "#2196f3";
+const cyan       = "#03a9f4";
+const teal       = "#00bcd4";
+const green      = "#009688";
+const lightGreen = "#4caf50";
+const lime       = "#8bc34a";
+const yellow     = "#cddc39";
+const amber      = "#ffeb3b";
+const orange     = "#ffc107";
+const deepOrange = "#ff9800";
+
+export const COLORS = [
+  red, purple, indigo, lightBlue, teal, lightGreen, yellow, orange,
+  pink, deepPurple, blue, cyan, green, lime, amber, deepOrange
+];
